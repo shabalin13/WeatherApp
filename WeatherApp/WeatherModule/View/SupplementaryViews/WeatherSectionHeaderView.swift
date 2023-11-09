b2afdcad69c19d33d47d65424cfaaeea506f1198
Shabalin13 <d.shabal1n12@gmail.com>
2023-11-09 22:12:20 +0300

    fixed WeatherSectionHeaderView (removed StackView, instead added AttributedText for label)

--- WeatherApp/WeatherModule/View/SupplementaryViews/WeatherSectionHeaderView.swift
+++ WeatherApp/WeatherModule/View/SupplementaryViews/WeatherSectionHeaderView.swift
@@ -11,29 +11,12 @@ class WeatherSectionHeaderView: UICollectionReusableView {
     
     static let reuseIdentifier = "WeatherSectionHeaderView"
     
-    lazy var stackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillProportionally
-        stackView.alignment = .center
-        stackView.spacing = 8
-        
-        return stackView
-    }()
-    
-    lazy var imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-//        imageView.backgroundColor = .red.withAlphaComponent(0.5)
-        
-        return imageView
-    }()
-    
-    lazy var titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .secondaryLabel
         label.font = .systemFont(ofSize: 15)
         label.numberOfLines = 1
+        label.textAlignment = .left
         
         return label
     }()
@@ -41,25 +24,14 @@ class WeatherSectionHeaderView: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-//        self.backgroundColor = .yellow.withAlphaComponent(0.3)
+        addSubview(titleLabel)
         
-        stackView.addArrangedSubview(imageView)
-        stackView.addArrangedSubview(titleLabel)
-        
-        addSubview(stackView)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
-            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
-            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-//            stackView.topAnchor.constraint(equalTo: self.topAnchor),
-//            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-//            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-//            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-//            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1)
-//            imageView.heightAnchor.constraint(equalTo: stackView.heightAnchor)
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
         ])
     }
     
@@ -69,8 +41,14 @@ class WeatherSectionHeaderView: UICollectionReusableView {
     }
     
     func configureHeader(imageName: String, titleName: String) {
-        imageView.image = UIImage(systemName: imageName)?.withTintColor(.secondaryLabel, renderingMode: .alwaysOriginal)
-        titleLabel.text = titleName.uppercased()
+        let titleString = NSMutableAttributedString()
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = UIImage(systemName: imageName)?.withTintColor(.secondaryLabel, renderingMode: .alwaysOriginal)
+        let imageString = NSAttributedString(attachment: imageAttachment)
+        titleString.append(imageString)
+        titleString.append(NSAttributedString(string: " \(titleName.uppercased())"))
+        
+        titleLabel.attributedText = titleString
     }
     
 }
