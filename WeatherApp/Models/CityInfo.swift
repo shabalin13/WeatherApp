3227705f1f8d1aa54390514ad69fa10a3250465e
Shabalin13 <d.shabal1n12@gmail.com>
2023-11-27 18:44:22 +0300

    removed CLLocation type from CityInfo and replaced be tuple (latitude: Double, longitude: Double)

--- WeatherApp/Models/CityInfo.swift
+++ WeatherApp/Models/CityInfo.swift
@@ -5,12 +5,10 @@
 //  Created by DIMbI4 on 27.11.2023.
 //
 
-import CoreLocation
-
 struct CityInfo {
     
     let displayLines: [String]
-    let location: CLLocation
+    let location: (latitude: Double, longitude: Double)
     
     enum RootKeys: String, CodingKey {
         case displayLines
@@ -34,7 +32,7 @@ extension CityInfo: Decodable {
         let locationContainer = try container.nestedContainer(keyedBy: LocationKeys.self, forKey: .location)
         let latitude = try locationContainer.decode(Double.self, forKey: .latitude)
         let longitude = try locationContainer.decode(Double.self, forKey: .longitude)
-        location = CLLocation(latitude: latitude, longitude: longitude)
+        location = (latitude, longitude)
         
     }
 }
